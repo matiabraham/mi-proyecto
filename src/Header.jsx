6a03358e01bdf31670fe39623653ba8e05d1efba
matiasabraham <matiasabrahammosconi@gmail.com>
2025-02-19 23:44:08 -0300

    Entrega 1 casi terminada

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+        <h1>Mi Ecommerce</h1>
+        <nav>
+          <a href="#">link 1</a>
+          <a href="#">link 2</a>
+          <a href="#">link 3</a>
+        </nav>
+      </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+  return (
+    <footer>
+        <p>&copy; Copyright 2025 Mati Abraham</p>
+      </footer>
+  )
+}
+
+export default Footer;
